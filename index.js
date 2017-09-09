32a02d9f41379ad4e11ee580a1ea5943251692d3
hkl1014 <linghangking93@hotmail.com>
2017-09-09 21:47:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,10 +17,12 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if (whisper(string)==string) {
     return `I can't hear you!`}
+    
   elseif(string=="I love you, Grandma."){
     return `I love you,too`
   }
-  } else {
+  
+   else {
 return `YES INDEED`
   }
 
